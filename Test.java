ab3cb7ee919bedf7bf490f925e76ffe3fa0274f5
StudentUni <57175112+StudentUni@users.noreply.github.com>
2020-03-30 19:38:43 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -49,7 +49,7 @@ public class Test {
                 int z = getVerseForCharacterNo(number);
                 System.out.println("result "+z);
                 System.out.println("Aya = "+getVerseInChapter(z));
-                System.out.println("number of surah "+getSurahNo(z));
+                System.out.println("name of surah "+getSurahName(z));
 
             } catch (Exception ex) {
             }
@@ -111,9 +111,10 @@ public class Test {
         return verseNo;
     }
     
-   public int getSurahNo(int result)
+   public String getSurahName(int result)
    {
          int id = 0;
+         String name_surah="";
          int number = 0, result_number = 0, aya = 0, c = 0;
 
          for(int i = 0; i <= numOfVerseInEverySurah.length; i++)
@@ -135,7 +136,348 @@ public class Test {
          }
 
          id = result_number + 1;
-         return id;
+         if(id==1){
+             name_surah="الفاتحة";
+         }
+         if(id==2){
+             name_surah="البقرة";
+         }
+         if(id==3){
+             name_surah="آل عمران";
+         }
+         if(id==4){
+             name_surah="النساء";
+         }
+         
+         if(id==5){
+             name_surah="المائد";
+         }
+         if(id==6){
+             name_surah="الأنعام";
+         }
+         if(id==7){
+             name_surah="الأعراف";
+         }
+         
+         if(id==8){
+             name_surah="الأنفال ";
+         }
+         if(id==9){
+             name_surah="التوبة";
+         }
+         if(id==10){
+             name_surah="يونس";
+         }
+         if(id==11){
+             name_surah="هود";
+         }
+         if(id==12){
+             name_surah="يوسف";
+         }
+         if(id==13){
+             name_surah="الرعد ";
+         }
+         if(id==14){
+             name_surah="إبراهيم";
+         }
+         if(id==15){
+             name_surah="الحجر";
+         }
+         if(id==16){
+             name_surah="النحل ";
+         }
+         if(id==17){
+             name_surah="الإسراء";
+         }
+         if(id==18){
+             name_surah="الكهف";
+         }
+         if(id==19){
+             name_surah="مريم";
+         }
+         if(id==20){
+             name_surah="طه";
+         }
+         if(id==21){
+             name_surah="الأنبياء";
+         }
+         if(id==22){
+             name_surah="الحج";
+         }
+         if(id==23){
+             name_surah="المؤمنون";
+         }
+         if(id==24){
+             name_surah="النور";
+         }
+         if(id==25){
+             name_surah="الفرقان";
+         }
+         if(id==26){
+             name_surah="الشعراء";
+         }
+         if(id==27){
+             name_surah="النمل";
+         }
+         if(id==28){
+             name_surah="القصص";
+         }
+         if(id==29){
+             name_surah="العنكبوت";
+         }
+         if(id==30){
+             name_surah="الروم ";
+         }
+         if(id==31){
+             name_surah="لقمان";
+         }
+         if(id==32){
+             name_surah="السجدة";
+         }
+         if(id==33){
+             name_surah="الأحزاب";
+         }
+         if(id==34){
+             name_surah="سبأ";
+         }
+         if(id==35){
+             name_surah="فاطر";
+         }
+         if(id==36){
+             name_surah="يس";
+         }
+         if(id==37){
+             name_surah="الصافات";
+         }
+         if(id==38){
+             name_surah="ص";
+         }
+         if(id==39){
+             name_surah="الزمر";
+         }
+         if(id==40){
+             name_surah="غافر ";
+         }
+         if(id==41){
+             name_surah="فصلت";
+         }
+         if(id==42){
+             name_surah="الشورى";
+         }
+         if(id==43){
+             name_surah="الزخرف";
+         }
+         if(id==44){
+             name_surah="الدخان";
+         }
+         if(id==45){
+             name_surah="الجاثية";
+         }
+         if(id==46){
+             name_surah="الأحقاف";
+         }
+         if(id==47){
+             name_surah="محمد";
+         }
+         if(id==48){
+             name_surah="الفتح";
+         }
+         if(id==49){
+             name_surah="الحجرات";
+         }
+         if(id==50){
+             name_surah="ق";
+         }
+         if(id==51){
+             name_surah="الذاريات";
+         }
+         if(id==52){
+             name_surah="الطور";
+         }
+         if(id==53){
+             name_surah="النجم";
+         }
+         if(id==54){
+             name_surah="القمر";
+         }
+         if(id==55){
+             name_surah="الرحمن";
+         }
+         if(id==56){
+             name_surah="الواقعة";
+         }
+         if(id==57){
+             name_surah="الحديد";
+         }
+         if(id==58){
+             name_surah="المجادلة";
+         }
+         if(id==59){
+             name_surah="الحشر";
+         }
+         if(id==60){
+             name_surah="الممتحنة";
+         }
+         if(id==61){
+             name_surah="الصف";
+         }
+         if(id==62){
+             name_surah="الجمعة";
+         }
+         if(id==63){
+             name_surah="المنافقون";
+         }
+         if(id==64){
+             name_surah="التغابن";
+         }
+         if(id==65){
+             name_surah="الطلاق";
+         }
+         if(id==66){
+             name_surah="التحريم";
+         }
+         if(id==67){
+             name_surah="الملك";
+         }
+         if(id==68){
+             name_surah="القلم";
+         }
+         if(id==69){
+             name_surah="القلم";
+         }
+         if(id==70){
+             name_surah="المعارج";
+         }
+         if(id==71){
+             name_surah="نوح ";
+         }
+         if(id==72){
+             name_surah="الجن";
+         }
+         if(id==73){
+             name_surah="المزمل";
+         }
+         if(id==74){
+             name_surah="المدثر";
+         }
+         if(id==75){
+             name_surah="القيامة";
+         }
+         if(id==76){
+             name_surah="الإنسان";
+         }
+         if(id==77){
+             name_surah="المرسلات";
+         }
+         if(id==78){
+             name_surah="النبأ";
+         }
+         if(id==79){
+             name_surah="النازعات";
+         }if(id==80){
+             name_surah="عبس";
+         }
+         if(id==81){
+             name_surah="التكوير";
+         }
+         if(id==82){
+             name_surah="الإنفطار";
+         }
+         if(id==83){
+             name_surah="المطففين";
+         }if(id==84){
+             name_surah="الإنشقاق";
+         }
+         if(id==85){
+             name_surah="البروج";
+         }
+         if(id==86){
+             name_surah="الطارق";
+         }
+         if(id==87){
+             name_surah="الأعلى";
+         }
+         if(id==88){
+             name_surah="الغاشية";
+         }
+         if(id==89){
+             name_surah="الفجر";
+         }
+         if(id==90){
+             name_surah="البلد";
+         }if(id==91){
+          name_surah="الشمس";   
+         }
+         if(id==92){
+          name_surah="الليل";   
+         }
+         if(id==93){
+          name_surah="الضحى";   
+         }if(id==94){
+          name_surah="الشرح";   
+         }
+         if(id==95){
+          name_surah="التين";   
+         }
+         if(id==96){
+          name_surah="العلق";   
+         }
+         if(id==97){
+          name_surah="القدر";   
+         }
+         if(id==98){
+          name_surah="البينة";   
+         }
+         if(id==99){
+          name_surah="الزلزلة";   
+         }
+         if(id==100){
+          name_surah="العاديات";   
+         }
+         if(id==101){
+          name_surah="القارعة";   
+         }
+         if(id==102){
+          name_surah="التكاثر";   
+         }
+         if(id==103){
+          name_surah="العصر";   
+         }
+         if(id==104){
+          name_surah="الهمزة";   
+         }
+         if(id==105){
+          name_surah="الفيل";   
+         }
+         if(id==106){
+          name_surah="قريش";   
+         }
+         if(id==107){
+          name_surah="الماعون";   
+         }
+         if(id==108){
+          name_surah="الكوثر ";   
+         }
+         if(id==109){
+          name_surah="الكافرون";   
+         }
+         if(id==110){
+          name_surah="النصر";   
+         }
+         if(id==111){
+          name_surah="المسد";   
+         }
+         if(id==112){
+          name_surah="الإخلاص";   
+         }
+         if(id==113){
+          name_surah="الفلق";   
+         }if(id==114){
+          name_surah="الناس";   
+         }
+         
+     
+         return name_surah;
    }
-  
+    
 }
