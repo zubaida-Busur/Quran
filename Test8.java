c3c480b248b001840256db7d123b5965e2927763
mzitow <hhbaraka0@gmail.com>
2020-03-31 02:27:34 +0200

    Changed the if-statements into enums

--- Test8.java
+++ Test8.java
@@ -4,7 +4,7 @@ public class Test8 {
 
         QuranData myQuran = new QuranData();
 
-        int finding = 30709;
+        int finding = 330709;
         System.out.printf("Character %S: Verse Index in quran :" + myQuran.getVerseForCharacterNo(finding) + "\nVerse No: "
                 + myQuran.getVerseInChapter(myQuran.getVerseForCharacterNo(finding)) + " of Surah: "
                 + myQuran.getSurahName(myQuran.getVerseForCharacterNo(finding)), finding);
